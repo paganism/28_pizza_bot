c1fe12a5b9e9efcda932b49792736825e22e7b7f
YuriyB <paganism@mail.ru>
2018-09-24 21:43:23 +0300

    modified server

--- config.py
+++ config.py
@@ -9,3 +9,5 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    BASIC_AUTH_USERNAME = 'admin'
+    BASIC_AUTH_PASSWORD = '1234'
--- server.py
+++ server.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template, g, url_for, redirect
+from flask import Flask, render_template, g, url_for, redirect, make_response, request
 from flask_sqlalchemy import SQLAlchemy
 from models import db
 from flask_migrate import Migrate
 from config import Config
 from functools import wraps
 from flask import request, Response
-from flask_admin import Admin
+from flask_admin import Admin, expose
 from flask_admin.contrib.sqla import ModelView
 from models import Pizza, Choices, db
 from flask_bootstrap import Bootstrap
+from flask_basicauth import BasicAuth
+from flask_admin.contrib import sqla
+from werkzeug.exceptions import HTTPException
 
 
 app = Flask(__name__)
@@ -17,10 +20,72 @@ app.config.from_object(Config)
 db.init_app(app)
 migrate = Migrate(app, db)
 bootstrap = Bootstrap(app)
+basic_auth = BasicAuth(app)
+
+
+# def auth_requires(f):
+#     @wraps(f)
+#     def decorated(*args, **kwargs):
+#         auth = request.authorization
+#         if auth and auth.username == 'username' and auth.password == 'password':
+#         # if auth and auth.username == app.config['BASIC_AUTH_USERNAME'] \
+#         #         and auth.password == app.config['BASIC_AUTH_PASSWORD']:
+#             return f(*args, **kwargs)
+#         return make_response('Could not verify your access level for that URL.\n'
+#                              'You have to login with proper credentials', 401,
+#                              {'WWW-Authenticate': 'Basic realm="Login Required"'})
+#
+#     return decorated
+
+class MyModelView(sqla.ModelView):
+
+
+    def is_accessible(self):
+        auth = request.authorization
+        if not auth or (auth.username != 'username' and auth.password != 'password'):
+            #print(auth)
+            #print(auth.username)
+            # return  make_response('Could not verify your access level for that URL.\n'
+            #                  'You have to login with proper credentials', 401,
+            #                  {'WWW-Authenticate': 'Basic realm="Login Required"'})
+            raise HTTPException('', Response(
+                "Введите корректные имя пользователя и пароль!",
+                401,
+                {'WWW-Authenticate': 'Basic realm="Login Required"'}
+            ))
+        print('HERE')
+        return True
+#
+#     @expose('/')
+#     @expose('/admin')
+#     def index(self):
+#         return self.render('index.html')
+#
+#     # def __init__(self, session):
+#     #     super(MicroBlogModelView, self).__init__(Pizza, session)
+
+class PizzaView(MyModelView):
+    column_searchable_list = ['title']
+    column_labels = dict(title='Название',
+                         description='Описание',
+                         choices='Варианты')
+
+class ChoicesView(MyModelView):
+    column_searchable_list = ['choice_title']
+    column_labels = dict(choice_title='Название',
+                         choice_price='Цена')
+
 
 admin = Admin(app, name='pizzeria', template_mode='bootstrap3')
-admin.add_view(ModelView(Pizza, db.session))
-admin.add_view(ModelView(Choices, db.session))
+
+admin.add_view(PizzaView(Pizza, db.session))
+admin.add_view(ChoicesView(Choices, db.session))
+
+
+# @app.route('/')
+# @basic_auth.required
+# def secret_view():
+#     return render_template('index.html')
 
 
 def check_auth(username, password):
@@ -63,7 +128,6 @@ def login_required(f):
 def secret_page():
 
     return render_template('admin/index.html')
-
 
 
 if __name__ == "__main__":
